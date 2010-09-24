cc33ab10575708adea2e2829ebc872bf873d62a5
kschwen <kschwen@A22926-NewHotness.local>
2010-09-24 12:59:24 -0700

    Now with 100% more rounded corners. Killed CSSification inside the shareify function, moved it to the example HTML

--- jquery.shareify.js
+++ jquery.shareify.js
@@ -117,13 +117,6 @@ shareifyHandlers = {
                     break;
             }
 
-            $this.css({
-                textAlign: 'center',
-                width: '100px',
-                background: '#000',
-                color: '#fff'
-            });
-
             $this.click(count_up);
 
         });
